087f3a2eadf85017fde55335c592064194b4d038
newwebdeveloper01 <nurullayevakbar30@gmail.com>
2023-10-05 16:21:33 +0500

    auth va language qo'shildi lakin hali tugailmadi

--- src/language/i18next.js
+++ src/language/i18next.js
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'ru',
+    fallbackLng: 'uz',
     debug: false,
     whitelist: Languages,
     interpolation: {
